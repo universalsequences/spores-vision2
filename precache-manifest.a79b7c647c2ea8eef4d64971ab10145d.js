394c77ee13c7a3e9200e374d9084f30bc71fdd95
Alec Resende <universalsequences@gmail.com>
2022-05-27 19:01:02 -0400

    build

---
+++ precache-manifest.a79b7c647c2ea8eef4d64971ab10145d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83b191e580918624041b",
+    "url": "/static/css/main.7fa7e332.chunk.css"
+  },
+  {
+    "revision": "83b191e580918624041b",
+    "url": "/static/js/main.748354dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d34d0125a12c372930c",
+    "url": "/static/js/2.4bb0b756.chunk.js"
+  },
+  {
+    "revision": "4af5067891631f9067fb591f300a3989",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95365d5f4e56235a64953c66f62b3447.js"
+  "/precache-manifest.a79b7c647c2ea8eef4d64971ab10145d.js"
 );
 
 workbox.clientsClaim();
