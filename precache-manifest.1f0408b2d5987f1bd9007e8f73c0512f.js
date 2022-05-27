0682f052b3b239d8ab401c58399d11a14d8a6e13
Hunter Martin <hunter.developer@aol.com>
2022-05-27 13:36:11 -0700

    new landing page

---
+++ precache-manifest.1f0408b2d5987f1bd9007e8f73c0512f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cae5917c27c02dea7dd2",
+    "url": "/static/css/main.8b7301c1.chunk.css"
+  },
+  {
+    "revision": "cae5917c27c02dea7dd2",
+    "url": "/static/js/main.c6c9e947.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c93b7ed7c03e5a69473c",
+    "url": "/static/js/2.7e320a79.chunk.js"
+  },
+  {
+    "revision": "02f953cf55a133405877993c5f379c8a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95365d5f4e56235a64953c66f62b3447.js"
+  "/precache-manifest.1f0408b2d5987f1bd9007e8f73c0512f.js"
 );
 
 workbox.clientsClaim();
