d3a18d57140025f7529678a14ac4a5e350003de5
Hunter Martin <hunter.developer@aol.com>
2022-05-31 12:19:15 -0700

    updated text + overview images

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55a1ee3eec42dce71035d0a11b45ca81.js"
+  "/precache-manifest.22e48f15e8374fb988ef575c4e87c3c0.js"
 );
 
 workbox.clientsClaim();
