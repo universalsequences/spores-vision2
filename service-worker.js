4f4192cb6e8297fffe6b8739c556502e829ad57e
Alec Resende <universalsequences@gmail.com>
2022-07-31 23:35:54 -0400

    build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92d8ec9393b97b2154fd0b244588ccde.js"
+  "/precache-manifest.91f55b27e22b6c943990862bc50ef778.js"
 );
 
 workbox.clientsClaim();
