813ac4ce88fca45323a6ed746d9bea21028411ea
Alec Resende <universalsequences@gmail.com>
2022-09-16 15:04:13 -0400

    build

---
+++ static/js/25.53c29589.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{1542:function(n,w){}}]);
+//# sourceMappingURL=25.53c29589.chunk.js.map
---
+++ static/js/26.a55259f5.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{1508:function(n,w){}}]);
+//# sourceMappingURL=26.a55259f5.chunk.js.map
